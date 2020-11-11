1bfe73de993a694aa883aa7ab721a7d69993f8c0
Petr Simecek <lamparna@gmail.com>
2020-11-11 01:40:24 +0100

    columns ui

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -23,15 +23,19 @@ ui <- fluidPage(
               value = 1,
               width = "60%"),
 
-  selectInput("topic", "Choose topic:", topics),
-  radioButtons("eval", "Quality of the tweet:", selected = "3", inline = TRUE,
+  column(3,
+  
+  selectInput("topic", "Choose topic:", topics, selectize = FALSE, size = length(topics)),
+  radioButtons("eval", "Quality of the tweet:", selected = "4", inline = TRUE,
                choices = c("1" = "1", "2" = "2", "3" = "3", "4" = "4", "5" = "5", "6" = "6", "7" = "7")),
   checkboxInput("duplicated", "Duplicated (already seen)"), 
   actionButton("goPrevious", "Previous"),
-  actionButton("goNext", "Next"),
+  actionButton("goNext", "Next")
+  ),
     
+  column(9,
   # Show a plot of the generated distribution
-  uiOutput("tweet"),
+  uiOutput("tweet")),
   
   conditionalPanel(condition = "input.n == 73",
       textInput("name", "To be saved under a name:", "")),
